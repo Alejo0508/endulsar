e7e8e73767d1ec0d33b45861f45c2e99fc6cda6e
alejandro montoya <alejandromg0805@gmail.com>
2020-11-29 20:04:31 -0500

    se agrega pagina de cupcakes

---
+++ cupcake.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="estulos.css">
+    <title>Cupcakes</title>
+    <link rel="shortcut icon" href="img/Logo.png" type="image/x-icon">
+</head>
+
+<body class="body">
+
+    <header>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-transparent ">
+            <img src="img/Logo.png" width="60" height="60" alt="" loading="lazy">
+            <a class="navbar-brand text-warning tamañotextonavbar margennavbar magenicon" href="index.html">EndulSar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+            </div>
+        </nav>
+
+    </header>
+
+    <main>
+
+        <div class="jumbotron jumbotron-fluid jumbotroncupcake">
+            <div class="container">
+                <h1 class="display-5">Cupcakes</h1>
+                <p class="lead textjumbtroncupcake">¡HOLA!, este espacio esta dedicado a cupcakes, elije el tuyo,
+                    personaliza el sabor y resibelos en tu casa.</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row justify-content-center">
+                <a class="btn btn-warning btn-lg textjumbtroncupcake" href="agregarproducto.php" role="button">Agrega un producto nuevo</a>
+            </div>
+        </div>
+
+
+    </main>
+
+
+    <footer>
+
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
